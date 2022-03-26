87f5b7721bd44741c9d59509e1ea6fb6e1af56cf
ankur182 <98750453+ankur182@users.noreply.github.com>
2022-03-26 10:41:26 +0530

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,19 @@
+text = 'mississippi'
+def make_dict(x):
+    dictionary = {}
+    for letter in x:
+        dictionary[letter] = 1 + dictionary.get(letter, 0)
+    return dictionary
+
+
+def most_frequent(text):
+    letters = [letter.lower() for letter in text if letter.isalpha()]
+    dictionary = make_dict(letters)
+    result = []
+    for key in dictionary:
+        result.append((dictionary[key], key))
+    result.sort(reverse=True)
+    for count, letter in result:
+        print letter, count
+
+most_frequent(text)
